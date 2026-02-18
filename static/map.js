a5c5e47584b2aead56c9c3766dc61333bb1ee9ac
owner <dimtosidis@gmail.com>
2026-02-18 10:53:04 +0200

    added files

--- static/map.js
+++ static/map.js
@@ -1,8 +1,27 @@
-const map = L.map("map").setView([41.1, 25.4], 7);
+const map = L.map("map",{layers: [lyr_satellite, lyr_streets]}).setView([41.1, 25.4], 7);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; OpenStreetMap contributors"
 }).addTo(map);
+
+var lyr_satellite = L.tileLayer(esri_url, {id: 'MapID', maxZoom: 20, tileSize: 512, zoomOffset: -1, attribution: esri_attribution});
+var lyr_streets   = L.tileLayer(mapbox_url, {id: 'mapbox/streets-v11', maxZoom: 28, tileSize: 512, zoomOffset: -1, attribution: mapbox_attribution});
+// var map = L.map('map', {
+//             center: [54.17747885048963, -6.337641477584839],
+//             zoom: 18,
+//             layers: [lyr_satellite, lyr_streets, lg_markers]
+//           });
+
+var baseMaps = {
+              "Streets": lyr_streets,
+              "Satellite": lyr_satellite
+          };
+// var overlayMaps = {
+//               "Markers": lg_markers,
+//           };
+
+L.control.layers(baseMaps).addTo(map);
+    // , overlayMaps).addTo(map);
 
 // map.invalidateSize();
 // setTimeout(() => {
@@ -11,7 +30,7 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 let marker = L.marker([50.4501, 30.5234],
   {alt: 'Kyiv'}).addTo(map) // "Kyiv" is the accessible name of this marker
-  .bindPopup('Kyiv, Ukraine is the birthplace of Leaflet!');//[]
+  .bindPopup('Kyiv is a nice city!');//[]
 
 // var popup = L.popup();
 //
@@ -24,15 +43,18 @@ let marker = L.marker([50.4501, 30.5234],
 //
 // map.on('click', onMapClick);
 
-var greenIcon = L.icon({
-    iconUrl: 'Pin_Dark.svg',
+var cloudIcon = L.icon({
+    iconUrl: 'static\\Pin_Dark.svg',
+//    shadowUrl:'',
 
     iconSize:     [38, 95], // size of the icon
+    //    shadowSize:   [50, 64], // size of the shadow
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    //    shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
-L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
+L.marker([51.5, -0.09], {icon: cloudIcon}).addTo(map);
 
 function clearMarkers() {
     markers.forEach(marker => map.removeLayer(marker));
